1415c0610fca24567fea1894876618f474cbe90c
David Hill <d_hill2@massbay.edu>
2025-02-18 12:17:41 -0500

    All files

--- PuzzleCategoryLoader.java
+++ PuzzleCategoryLoader.java
@@ -1,2 +1,12 @@
+import java.io.*;
+import java.util.*;
+
 public class PuzzleCategoryLoader {
+    private Map<String, List<String>> categories;
+    public PuzzleCategoryLoader(String filePath) {
+        categories = new HashMap<>();
+        loadCategories(filePath);
+    }
+
+
 }
