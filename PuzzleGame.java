d3e749ef92fd88e4d2b3b76f6d664c7446cda6af
David Hill <d_hill2@massbay.edu>
2025-02-20 11:28:38 -0500

    rough draft of PuzzleGame.java

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -1,2 +1,13 @@
-public class PuzzleGame {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class PuzzleGame extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        String filePath = "PuzzleCategories.csv";
+        new PuzzleGridController(primaryStage, filePath);
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
