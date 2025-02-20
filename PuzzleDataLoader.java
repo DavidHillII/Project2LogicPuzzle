93c1ae32a2e198ea5f36bfa93dc217db7e0c86b8
ValeriesUtopia <valerieholland524@gmail.com>
2025-02-20 12:42:23 -0500

    Update PuzzleDataLoader.java

--- PuzzleDataLoader.java
+++ PuzzleDataLoader.java
@@ -1,16 +1,31 @@
 //Valerie
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+
 
 public class PuzzleDataLoader {
-    private Map<String, String> correctPairs = new HashMap<>();
+    private ArrayList<String> correctPairs = new ArrayList<>();
+    
 
-    public PuzzleDataLoader(String fileName) throws FileNotFoundException, IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
-            // read catergories of first row
-            String[] categoryNames = br.readLine().split(",");
+    public void loadCSV(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] pair = line.split(",");
+                if (pair.length == 2) {
+                    correctPairs.add(pair[0] + "," + pair[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }    
-}
+        
+    
+    }
+    public boolean isCorrectPair(String item1, String item2) {
+        return correctPairs.contains(item1 + "-" + item2) || correctPairs.contains(item2 + "-" + item1);
+    }
+      
+    }
