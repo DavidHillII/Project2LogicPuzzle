8611c0642fa367183fce3b79d054248a723667a3
ValeriesUtopia <valerieholland524@gmail.com>
2025-02-24 01:22:39 -0500

    Update PuzzleGame.java

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -1,12 +1,20 @@
 //David
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+// fixed by Valerie 2/24 added ioexpection and fixed start
 public class PuzzleGame extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        String filePath = "PuzzleCategories.csv";
-        new PuzzleGridController(primaryStage, filePath);
+    public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("Puzzle_Game.fxml")); // Load FXML
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
     }
     public static void main(String[] args) {
         launch(args); //starts the window
