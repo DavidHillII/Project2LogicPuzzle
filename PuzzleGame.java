1265db6b89caea6f937269175eb7694c38a67ec9
ValeriesUtopia <valerieholland524@gmail.com>
2025-02-24 02:04:56 -0500

    Update PuzzleGame.java

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// fixed by Valerie 2/24 added ioexpection and fixed start
+// fixed by Valerie 2/24 added ioexpection and fixed start with tests
 public class PuzzleGame extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -18,5 +18,22 @@ public class PuzzleGame extends Application {
     }
     public static void main(String[] args) {
         launch(args); //starts the window
+        
+                // tests CsvSolutions.txt into console
+        PuzzleDataLoader dataLoader = new PuzzleDataLoader("C:\\Users\\valer\\Desktop\\CS225\\Project2\\CSVSolutions.txt");
+        for (String pair : dataLoader.getCorrectPairs()) {
+            System.out.println(pair);
+        }
+        // tests CSVCategories.txt into console
+        PuzzleCategoryLoader categoryLoader = new PuzzleCategoryLoader("C:\\Users\\valer\\Desktop\\CS225\\Project2\\CSVCategories.txt");
+        for (String category : categoryLoader.getCategories()) {
+            System.out.println(category);
+        }
+            // tests CSVHints.txt to be added by Anthony
+
+        
+    }
+        public PuzzleGame() {
+     
     }
 }
