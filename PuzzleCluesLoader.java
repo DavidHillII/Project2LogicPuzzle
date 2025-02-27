4e76604d93059117a4bda000dad4c7be5d9eda65
ValeriesUtopia <valerieholland524@gmail.com>
2025-02-27 14:23:54 -0500

    Update PuzzleCluesLoader.java

--- PuzzleCluesLoader.java
+++ PuzzleCluesLoader.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class PuzzleCluesLoader { //class for loading the list of clues from a csv file
     private List<String> clues_List; //Created the list for the clues
 
-    public PuzzleCluesLoader(String filePath) {
+    public PuzzleCluesLoader(String fileName) {
         clues_List = new ArrayList<>(); //Creates an arrayList
-        loadClues(filePath); //load the clues from the file
+        loadClues(fileName); //load the clues from the file
     }
 
-    public void loadClues(String filePath) { //function for loading clues
-        try (Scanner scanner = new Scanner(new File(filePath))) { //Creates a scanner and implements a try catch in order to successfully read through a csv file.
+    public void loadClues(String fileName) { //function for loading clues
+        try (Scanner scanner = new Scanner(new File(fileName))) { //Creates a scanner and implements a try catch in order to successfully read through a csv file.
             while (scanner.hasNextLine()) { //Reads the file line of the csv file
                 String line = scanner.nextLine().trim(); //Stores the text in a string.
                 if (!line.isEmpty()) { //So long as there is another clue in the csv file add the string into the arrayList.
@@ -34,3 +34,4 @@ public class PuzzleCluesLoader { //class for loading the list of clues from a cs
         }
     }
 }
+
